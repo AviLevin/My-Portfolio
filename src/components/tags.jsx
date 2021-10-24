9d3c028b4631a23a256908b497d3774cb80e6c36
avremi89 <avremi89@gmail.com>
2021-10-24 19:37:01 +0300

    d
    d

--- src/components/tags.jsx
+++ src/components/tags.jsx
@@ -11,7 +11,7 @@ export default class About extends Component {
                             <div className="col-md-12">
                                 <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
                                     <div className="col-md-12">
-                                        <div className="about-desc">
+                                        <div className="about-desc"> 
                                             <img align="left" alt="React" width="26px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png" />
                                             <img align="left" alt="React" width="26px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/redux/redux.png" />
                                             <img align="left" alt="HTML5" width="26px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png" />
