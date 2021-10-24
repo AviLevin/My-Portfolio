cdc880cb7d9e331feb866ade1f2f427398523644
avremi89 <avremi89@gmail.com>
2021-10-24 19:37:42 +0300

    d
    d

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
-import Sidebar from './components/sidebar'
-import Introduction from './components/introduction'
-import About from './components/about'
-import Timeline from './components/timeline'
-import Projects from './components/projects';
-import Tags from './components/tags';
-
+import React, { Component } from "react";
+import "./App.css";
+import Sidebar from "./components/sidebar";
+import Introduction from "./components/introduction";
+import About from "./components/about";
+import Timeline from "./components/timeline";
+import Projects from "./components/projects";
+import Tags from "./components/tags";
 
 class App extends Component {
   render() {
     return (
       <div id="colorlib-page">
         <div id="container-wrap">
-         	<Sidebar></Sidebar>
-				<div id="colorlib-main">
-					<Introduction></Introduction>
-          <Projects></Projects>
-					<About></About>
-          <Tags></Tags>
-          
-					<Timeline></Timeline>
-          	</div>
-      	</div>
+          <Sidebar></Sidebar>
+          <div id="colorlib-main">
+            <Introduction></Introduction>
+            <Projects></Projects>
+            <About></About>
+            <Tags></Tags>
+
+            <Timeline></Timeline>
+          </div>
+        </div>
       </div>
     );
   }
